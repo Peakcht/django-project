2103d0f72bac5ba124758f19e47314cf5f83e4cf
Peakcht <151814595+Peakcht@users.noreply.github.com>
2025-09-28 19:43:21 +0700

    Add files via upload

--- food/urls.py
+++ food/urls.py
@@ -1,30 +1,24 @@
-from django.urls import path
-from . import views
-from django.conf import settings
-from django.conf.urls.static import static
-from django.contrib.auth import views as auth_views
+"""
+URL configuration for mysite_project project.
+
+The `urlpatterns` list routes URLs to views. For more information please see:
+    https://docs.djangoproject.com/en/5.0/topics/http/urls/
+Examples:
+Function views
+    1. Add an import:  from my_app import views
+    2. Add a URL to urlpatterns:  path('', views.home, name='home')
+Class-based views
+    1. Add an import:  from other_app.views import Home
+    2. Add a URL to urlpatterns:  path('', Home.as_view(), name='home')
+Including another URLconf
+    1. Import the include() function: from django.urls import include, path
+    2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
+"""
+from django.contrib import admin
+from django.urls import path, include
+
 
 urlpatterns = [
-    path('login/', auth_views.LoginView.as_view(template_name='login.html'), name='login'),
-    path('', views.homepage, name='homepage'),
-    path('order_page/<str:order_id>/', views.order_page, name='order_page'),
-    path('order/<str:order_id>/submit/', views.submit_order, name='submit_order'),
-    path('order_status/<str:order_id>/', views.order_status, name='order_status'),
-    path('change_package/<str:order_id>/', views.change_package, name='change_package'),
-    path('kitchen_order/', views.kitchen_order, name='kitchen_order'),
-    path('waiter_order/', views.waiter_order, name='waiter_order'),
-    path('checkout_list/', views.checkout_list, name='checkout_list'), #add
-    path('checkout/<str:order_id>/', views.checkout, name='checkout'),
-    path('receipt/<str:order_id>/', views.receipt, name='receipt'), #checkout receipt
-    path('submit_receipt/', views.submit_receipt, name='submit_receipt'),#submit transaction
-    path('receipt_view/<uuid:invoice_id>/', views.receipt_view, name='receipt_view'), #use to view submit photo receipt
-    path('invoice_list/', views.invoice_list_view, name='invoice_list'),
-    path('dashboard/', views.dashboard, name='dashboard'),
-    path('session_expired/', views.session_expired_page, name='session_expired'),
-    #path('staff/', views.staff, name='dashboard-staff'),
-    #path('product/', views.product, name='dashboard-product'),
-    #path('order/', views.order, name='dashboard-order'),
+    path('admin/', admin.site.urls),
+    path('', include('food.urls')),
 ]
-if settings.DEBUG:  # Serve media files only in debug mode
-    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
-    urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
