d35338aa7912db0e2ab42cc9b83036b27b2887e8
Peakcht <151814595+Peakcht@users.noreply.github.com>
2025-09-28 19:04:16 +0700

    Update urls.py

--- food/urls.py
+++ food/urls.py
@@ -2,8 +2,10 @@ from django.urls import path
 from . import views
 from django.conf import settings
 from django.conf.urls.static import static
+from django.contrib.auth import views as auth_views
 
 urlpatterns = [
+    path('login/', auth_views.LoginView.as_view(template_name='login.html'), name='login'),
     path('', views.homepage, name='homepage'),
     path('order_page/<str:order_id>/', views.order_page, name='order_page'),
     path('order/<str:order_id>/submit/', views.submit_order, name='submit_order'),
